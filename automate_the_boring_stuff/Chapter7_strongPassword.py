3e43f4c68d9d8dac6ca765f90c9d43334b57489f
Cynthia Novac-Yu <cynthia.novacyu@gmail.com>
2018-09-20 11:03:42 -0700

    fixed regex to ensure both capital and lowercase chars are included. added loop for password check

--- automate_the_boring_stuff/Chapter7_strongPassword.py
+++ automate_the_boring_stuff/Chapter7_strongPassword.py
@@ -1,21 +1,38 @@
 #! python3
-# detect if a password is strong (must contain a number and a capital letter) and has 8 chars
+# detect if a password is strong (must contain a number, lowercase, and capital letter) and has 8 chars
 
-import re
+import re, sys
 
 # check for capital letters, lowercase letters and a number
-passRegex = re.compile(r'\w*[A-Z]+\w*\d+')
+passRegex = re.compile(r'''(
+    ^                   #start anchor
+    (?=.*[A-Z])         #positive look ahead assertion to ensure it contains capital letter
+    (?=.*[a-z]+)        #positive look ahead assertion to ensure it contains a lowercase letter
+    (?=.*\d+)           #positive look ahead assertion to ensure it contains a number
+    .{8,}               #ensure string has length of at least 8
+    $                   #end anchor
+    )''', re.VERBOSE)   #VERBOSE allows regex to be broken into multiple lines
 
-print('Type in your new password:')
-new_pass = input()
-#mo = passRegex.search(new_pass)
+new_pass = ''
 
-#check if the password is at least 8 chars
-if len(new_pass) < 8:
-    print('Passwords must be at least 8 characters. Please enter in a new password.')
-# if Regex does not match, prompt user to try again
-elif passRegex.search(new_pass) == None:
-    print('Password must contain a number and capital letter. Please enter a new password')
-else:    
-    #print(mo.group())
-    print('Strong password! Password accepted')
+def new_password():
+    print('Type in your new password:')
+    new_pass = input()
+    return new_pass
+    #mo = passRegex.search(new_pass)
+
+def check_password(new_pass):
+    #check if the password is at least 8 chars
+    if len(new_pass) < 8:
+        print('Passwords must be at least 8 characters. Please enter in a new password.')
+    # if Regex does not match, prompt user to try again
+    elif passRegex.search(new_pass) == None:
+        print('Password must contain a number, lowercase, and capital letter. Please enter a new password')
+    else:    
+        #print(mo.group())
+        print('Strong password! Password accepted')
+        return False
+
+while True:
+    if check_password(new_password()) == False:
+        break
