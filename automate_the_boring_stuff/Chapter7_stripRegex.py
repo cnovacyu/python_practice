b81702cd8cee0c0266b1938a33ea4c256cea700f
Cynthia Novac-Yu <cynthia.novacyu@gmail.com>
2018-10-19 17:12:37 -0700

    initial code for stripping white space and chars from a phrase using regex

--- automate_the_boring_stuff/Chapter7_stripRegex.py
+++ automate_the_boring_stuff/Chapter7_stripRegex.py
@@ -4,3 +4,31 @@
 
 import re
 
+print('Type in the phrase you would like to modify and remove white space from:')
+phrase = input()
+# phrase = '    hello   '
+
+print('''Type in the characters you would like to remove from your phrase (not case sensitive):''')
+strip = input()
+#strip = 'LL'
+
+print('\n \nThe phrase you would like to modify and remove white space from is: \n'
++ phrase + '\n\n' + 'The characters you would like to remove from your phrase are: \n' + strip + '\n')
+
+# Remove any white space characters from an inputted phrase
+def trimPhrase(phrase):
+    trimRegex = re.compile(r'\S+')
+    mo1 = trimRegex.search(phrase)
+    trim_phrase = mo1.group()
+    #print(len(mo.group()))
+    #print(len(phrase))
+    return(trim_phrase)
+
+def stripPhrase(trim_phrase):
+    stripRegex = re.compile(re.escape(strip), re.IGNORECASE)
+    strip_phrase = stripRegex.sub('', trim_phrase)
+    print(strip_phrase)
+    print(len(strip_phrase))
+
+#enterPhrase()
+stripPhrase(trimPhrase(phrase))
