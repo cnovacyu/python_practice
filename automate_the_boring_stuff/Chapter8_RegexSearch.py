8db6742037f46580cdb6f7a9d820b66b8deb6be9
Cynthia Novac-Yu <cynthia.novacyu@gmail.com>
2018-10-28 18:48:15 -0700

    initial code for regex searching text files in certain directories

---
+++ automate_the_boring_stuff/Chapter8_RegexSearch.py
@@ -0,0 +1,30 @@
+#! python 3
+# Open all .txt files in a folder and search for any line that matches
+# user input. Results are printed to the terminal.
+
+import os, re
+
+# Prompt user for a search phrase
+print('Enter the phrase you would like to search in text files: ')
+search = input()
+
+# Regex set to phrase entered by user
+searchRegex = re.compile(search, re.IGNORECASE)
+
+# Change working directory to location containing text files to search
+os.chdir(r'C:\Users\cnovacy\Documents\01 - Projects\python_practice\automate_the_boring_stuff\Test_Files')
+
+# Check each file in the directory above for search phrase entered by user
+for filename in os.listdir(r'C:\Users\cnovacy\Documents\01 - Projects\python_practice\automate_the_boring_stuff\Test_Files'):
+    # Only search text files
+    if filename.endswith('.txt'):
+        # Troubleshoot to check files
+        #print(filename)
+        # Open each file and read content
+        checkFile = open(filename, 'r')
+        fileContent = checkFile.read()
+        # Search each file for the Regex phrase entered by the user
+        #mo = searchRegex.search(fileContent)
+        mo = searchRegex.findall(fileContent)
+        print(filename + ' contains your search phrase: \"' + search + '\" ' + str(len(mo)) + ' times')
+    checkFile.close()
