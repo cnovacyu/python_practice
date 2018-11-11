25ce1723591f63d4daa36d820ad3f21f7c1d0266
Cynthia Novac-Yu <cynthia.novacyu@gmail.com>
2018-11-10 23:45:42 -0800

    added args to specify file size

--- automate_the_boring_stuff/Chapter9_DeletingLargeFiles.py
+++ automate_the_boring_stuff/Chapter9_DeletingLargeFiles.py
@@ -4,19 +4,30 @@
 
 import os, argparse
 
-# create arg for directory to search for large files
+# create args for directory and file sizes to search for
 parser = argparse.ArgumentParser()
 parser.add_argument("src", help="source directory to check for large file sizes")
+parser.add_argument("file_size", type=int, help='choose size of file')
+parser.add_argument("size_type", choices=['B', 'KB', 'MB', 'GB', 'TB', 'PB'], help='choose file size type')
 args = parser.parse_args()
 
-# set arg to variables
+# set args to variables
 src = args.src
+file_size = args.file_size
+size_type = str(args.size_type)
 
-# walk through a specified directory and check for files larger than 100 MB
-# print filename and absolute path of large files
+# convert os.path.getsize from bytes into size type specified in arg
+convert_dict = {'B':1, 'KB':10, 'MB':20, 'GB':30, 'TB':40, 'PB':50}
+factor = convert_dict[size_type]
+
+# walk through a specified directory and check for files larger than file size
+# specified. Print filename and absolute path of large files
+print()
+print('Checking for files that are larger than ' + str(file_size) + ' ' + size_type + ' in: ' + src)
+
 for foldername, subfolders, filenames in os.walk(src):
     for filename in filenames:
-        if (os.path.getsize(os.path.join(src, filename)) >> 20) > 100:
-            print(filename + ':'),
-            print(os.path.getsize(os.path.join(src, filename)) >> 20)
-        
+        if (os.path.getsize(os.path.join(src, filename)) >> factor) > file_size:
+            print(filename + ': ', end='')
+            print(os.path.getsize(os.path.join(src, filename)) >> factor, end='')
+            print(' ' + size_type)
