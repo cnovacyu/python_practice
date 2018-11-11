a8f154b2c152eafbe206f29d6b9ffe9107ec7094
Cynthia Novac-Yu <cynthia.novacyu@gmail.com>
2018-11-10 18:21:37 -0800

    added comments

--- automate_the_boring_stuff/Chapter9_SelectiveCopy.py
+++ automate_the_boring_stuff/Chapter9_SelectiveCopy.py
@@ -4,19 +4,22 @@
 
 import shutil, os, argparse
 
+# create args that need to be populated to run script
+# args for file extenstion, source directory, and destination directory 
+# for copied specified files
 parser = argparse.ArgumentParser(description="copy specified type of files to new folder")
 parser.add_argument("file_type", help="file extension type")
 parser.add_argument("src", help="source directory to copy files from")
 parser.add_argument("des", help="destination directory to save copied files")
 args = parser.parse_args()
 
+# set args to variables
 file_type = args.file_type
 src = args.src
 des = args.des
 
-# Change working directory to location containing text files to search
-#os.chdir(r'C:\Users\cnovacy\Documents\01 - Projects\python_practice\automate_the_boring_stuff\Test_Files\cats')
-
+# walk through a specified directory and copy file if it is the specified
+# file extension into a destination folder specified
 for foldername, subfolders, filenames in os.walk(src):
     for filename in filenames:
         if filename.endswith(file_type):
