3bbb9ecbf8c80a81b21bd645ebae0e549320c1ba
Cynthia Novac-Yu <cynthia.novacyu@gmail.com>
2018-11-10 18:14:10 -0800

    added argparse to enter script arguments

--- automate_the_boring_stuff/Chapter9_SelectiveCopy.py
+++ automate_the_boring_stuff/Chapter9_SelectiveCopy.py
@@ -2,13 +2,23 @@
 # Walk through a folder tree and search for files with specific file
 # extensions. Copy the files to a new folder.
 
-import shutil, os
+import shutil, os, argparse
+
+parser = argparse.ArgumentParser(description="copy specified type of files to new folder")
+parser.add_argument("file_type", help="file extension type")
+parser.add_argument("src", help="source directory to copy files from")
+parser.add_argument("des", help="destination directory to save copied files")
+args = parser.parse_args()
+
+file_type = args.file_type
+src = args.src
+des = args.des
 
 # Change working directory to location containing text files to search
-os.chdir(r'C:\Users\cnovacy\Documents\01 - Projects\python_practice\automate_the_boring_stuff\Test_Files\cats')
+#os.chdir(r'C:\Users\cnovacy\Documents\01 - Projects\python_practice\automate_the_boring_stuff\Test_Files\cats')
 
-for foldername, subfolders, filenames in os.walk(r'C:\Users\cnovacy\Documents\01 - Projects\python_practice\automate_the_boring_stuff\Test_Files\cats'):
+for foldername, subfolders, filenames in os.walk(src):
     for filename in filenames:
-        if filename.endswith('.txt'):
-            src = foldername + '\\'+ filename
-            shutil.copy(src, r'C:\Users\cnovacy\Documents\01 - Projects\python_practice\automate_the_boring_stuff\Test_Files\cats_new')
+        if filename.endswith(file_type):
+            start = foldername + '\\'+ filename
+            shutil.copy(start, des)
