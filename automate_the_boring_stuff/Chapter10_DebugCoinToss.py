aaa7866c7f825bd203e1f324c02e71783a5ef635
Cynthia Novac-Yu <cynthia.novacyu@gmail.com>
2018-11-28 21:28:55 -0800

    fixed bugs in coin toss program

--- automate_the_boring_stuff/Chapter10_DebugCoinToss.py
+++ automate_the_boring_stuff/Chapter10_DebugCoinToss.py
@@ -1,15 +1,20 @@
 import random
+
 guess = ''
+
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
+
 toss = random.randint(0, 1) # 0 is tails, 1 is heads
-if toss == guess:
+toss_results = {0:"tails",1: "heads"}
+
+if toss_results[toss] == guess:
     print('You got it!')
 else:
     print('Nope! Guess again!')
-    guesss = input()
-    if toss == guess:
+    guess = input()
+    if toss_results[toss] == guess:
         print('You got it!')
     else:
         print('Nope. You are really bad at this game.')
